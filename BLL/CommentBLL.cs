6e175ffa44253bd7763cd3657b11e146b6d3da39
纪伟 薛 <406955883@qq.com>
2019-03-14 09:58:56 +0800

    Custom Comment bll层编写

---
+++ BLL/CommentBLL.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using DataService;
+using MODEL;
+
+namespace BLL
+{
+    public class CommentBLL : ICommon<Comment>
+    {
+        #region 单例
+        private static CommentBLL bll = null;
+        private CommentBLL()
+        {
+        }
+        public static CommentBLL CreateCommentBll()
+        {
+            if (bll == null)
+            {
+                bll = new CommentBLL();
+            }
+            return bll;
+        }
+        #endregion
+        public int Add(Comment t)
+        {
+            return CommentDAL.CreateCommentDal().Add(t);
+        }
+
+        public int Del(int Id)
+        {
+            return CommentDAL.CreateCommentDal().Del(Id);
+        }
+
+        public List<Comment> Show()
+        {
+            return CommentDAL.CreateCommentDal().Show();
+        }
+
+        public Comment ShowById(int Id)
+        {
+            return CommentDAL.CreateCommentDal().ShowById(Id);
+        }
+
+        public int Upd(Comment t)
+        {
+            return CommentDAL.CreateCommentDal().Upd(t);
+        }
+    }
+}
---
+++ BLL/CustomBLL.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataService;
+using MODEL;
+using DAL;
+
+namespace BLL
+{
+    //Custombll
+    public class CustomBLL : ICommon<Custom>
+    {
+        #region 单例
+        private static CustomBLL bll = null;
+        private CustomBLL()
+        {
+        }
+        public static CustomBLL CreateCustomBll()
+        {
+            if (bll == null)
+            {
+                bll = new CustomBLL();
+            }
+            return bll;
+        }
+        #endregion
+        
+        public int Add(Custom t)
+        {
+            return CustomDAL.CreateCustomDal().Add(t);
+        }
+
+        public int Del(int Id)
+        {
+            return CustomDAL.CreateCustomDal().Del(Id);
+        }
+
+        public List<Custom> Show()
+        {
+            return CustomDAL.CreateCustomDal().Show();
+        }
+
+        public Custom ShowById(int Id)
+        {
+            return CustomDAL.CreateCustomDal().ShowById(Id);
+        }
+
+        public int Upd(Custom t)
+        {
+            return CustomDAL.CreateCustomDal().Upd(t);
+        }
+    }
+}
