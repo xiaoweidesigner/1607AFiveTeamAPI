8b28f97ec1ff6dd6854709a358cecb35543d8daf
纪伟 薛 <406955883@qq.com>
2019-03-14 08:52:30 +0800

    大叔

--- DAL/Commentdal.cs
+++ DAL/Commentdal.cs
@@ -3,73 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MODEL;
-using DataService;
 
 namespace DAL
 {
-    public class Commentdal : ICommon<Comment>
+   public  class CommentDAL
     {
-        private static Commentdal dal = null;
-        private Commentdal()
-        {
-        }
-
-        public static Commentdal CreateCommentDal()
-        {
-            if (dal == null)
-            {
-                dal = new Commentdal();
-            }
-            return dal;
-        }
-
-        public int Add(Comment t)
-        {
-            using (MyDbContext db = new MyDbContext())
-            {
-                db.Database.CreateIfNotExists();
-                db.Comment.Add(t);
-                return db.SaveChanges();
-            }
-        }
-
-        public int Del(int Id)
-        {
-            using (MyDbContext db = new MyDbContext())
-            {
-                db.Database.CreateIfNotExists();
-                db.Comment.Remove(db.Comment.Where(s => s.CoId == Id).FirstOrDefault());
-                return db.SaveChanges();
-            }
-        }
-
-        public List<Comment> Show()
-        {
-            using (MyDbContext db = new MyDbContext())
-            {
-                db.Database.CreateIfNotExists();
-                return db.Comment.ToList();
-            }
-        }
-
-        public Comment ShowById(int Id)
-        {
-            using (MyDbContext db = new MyDbContext())
-            {
-                db.Database.CreateIfNotExists();
-                return db.Comment.Where(s => s.CoId == Id).FirstOrDefault();
-            }
-        }
-
-        public int Upd(Comment t)
-        {
-            using (MyDbContext db = new MyDbContext())
-            {
-                db.Database.CreateIfNotExists();
-                db.Entry<Comment>(t).State = System.Data.Entity.EntityState.Modified;
-                return db.SaveChanges();
-            }
-        }
     }
 }
