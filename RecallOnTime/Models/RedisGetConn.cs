1822b0cded16bccfe65434a820e0e900140737c0
纪伟 薛 <406955883@qq.com>
2019-03-28 20:41:46 +0800

    xjw API

---
+++ RecallOnTime/Models/RedisGetConn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using StackExchange.Redis;
+
+namespace RecallOnTime.Models
+{
+    public class RedisGetConn
+    {
+        private static ConfigurationOptions configuration = ConfigurationOptions.Parse("127.0.0.1:6379");
+        private static ConnectionMultiplexer redsConn;  //连接redis源
+        private static readonly object Locker = new object();//创建一个锁
+        public static ConnectionMultiplexer GetConn()
+        {
+            if (redsConn == null)
+            {
+                lock (Locker)
+                {
+                    if (redsConn == null || redsConn.IsConnected)
+                    {
+                        redsConn = ConnectionMultiplexer.Connect(configuration);
+                    }
+                }
+            }
+            return redsConn;
+        }
+    }
+}
